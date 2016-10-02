1bfbdc973ff0661de0cd09ce0fdc670284945a83
weicong-me <100055958@student.swin.edu.au>
2016-10-03 09:48:00 +1100

    Finish Unit Test About AI Level Setting

--- battleships_cs/src/UnitTest/NUnitTest_AI.cs
+++ battleships_cs/src/UnitTest/NUnitTest_AI.cs
@@ -9,9 +9,21 @@ namespace Application
 		public void TestCase()
 		{
 			GameController.StartGame ();
-			AIOption expected__aiSetting = AIOption.Medium;
-			AIOption actual__aiSetting = GameController._aiSetting;
-			Assert.AreEqual (expected__aiSetting, actual__aiSetting);
+
+			AIOption expected__aiSetting_default = AIOption.Easy;
+			AIOption actual__aiSetting_default = GameController._aiSetting;
+			Assert.AreEqual (expected__aiSetting_default, actual__aiSetting_default);
+
+
+			GameController.SetDifficulty (AIOption.Medium);
+			AIOption expected__aiSetting_M = AIOption.Medium;
+			AIOption actual__aiSetting_M = GameController._aiSetting;
+			Assert.AreEqual (expected__aiSetting_M, actual__aiSetting_M);
+
+			GameController.SetDifficulty (AIOption.Hard);
+			AIOption expected__aiSetting_H = AIOption.Hard;
+			AIOption actual__aiSetting_H = GameController._aiSetting;
+			Assert.AreEqual (expected__aiSetting_H, actual__aiSetting_H);
 		}
 	}
 }
