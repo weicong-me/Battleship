9ad09b27e3901ebb02f3d86f8636e41d1d43901c
Emperor-Justinian <emperorjustinian_i-github@yahoo.com.au>
2016-10-02 17:03:15 +1100

    Added Unit Tests to test for the Resources bug

---
+++ battleships_cs/src/UnitTests/ResourcesTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace MyGame
+{
+	[TestFixture ()]
+	public class ResourcesTests
+	{
+		[Test ()]
+		public void TestResourcesDirectory ()
+		{
+			Assert.IsTrue (Directory.Exists ("Resources"));
+		}
+
+		[Test ()]
+		public void TestResourcesHighscores ()
+		{
+			Assert.IsTrue (File.Exists ("Resources/highscores.txt"));
+		}
+
+		[Test ()]
+		public void TestResourcesDeploy ()
+		{
+			Assert.IsTrue (File.Exists ("Resources/images/deploy.jpg"));
+		}
+	}
+}
+
