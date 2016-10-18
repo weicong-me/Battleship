232ca5857eadb0e8fc8c53c750b46775d428646b
Malith077 <100671040@student.swin.edu.au>
2016-10-18 15:02:43 +1100

    add new debugged unit test file

--- battleships_cs/src/UnitTests/TestHits.cs
+++ battleships_cs/src/UnitTests/TestHits.cs
@@ -10,7 +10,18 @@ namespace MyGame
 		{
 			BattleShipsGame battle = new BattleShipsGame ();
 			Player test = new Player (battle);
+
+			//moving ships into invalid positions and making sure they are off the grid
+
+			try { test.PlayerGrid.MoveShip (10, 0, ShipName.Tug, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 1, ShipName.Submarine, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 2, ShipName.Destroyer, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 3, ShipName.Battleship, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 4, ShipName.AircraftCarrier, Direction.LeftRight); } catch (Exception ex) { }
+
+
 			AttackResult actual = test.PlayerGrid.HitTile (0, 0);
+
 
 			Assert.AreEqual ("missed", actual.Text, "should return as 'missed' ");
 		}
@@ -20,7 +31,16 @@ namespace MyGame
 			BattleShipsGame battle = new BattleShipsGame ();
 			Player test = new Player (battle);
 
-			test.PlayerGrid.MoveShip (0, 0, ShipName.Submarine, Direction.LeftRight);
+			//moving ships into invalid positions and making sure they are off the grid
+
+			try { test.PlayerGrid.MoveShip (10, 0, ShipName.Tug, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 1, ShipName.Submarine, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 2, ShipName.Destroyer, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 3, ShipName.Battleship, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 4, ShipName.AircraftCarrier, Direction.LeftRight); } catch (Exception ex) { }
+
+
+			try { test.PlayerGrid.MoveShip (0, 0, ShipName.Submarine, Direction.LeftRight); } catch (Exception ex){ }
 
 			AttackResult actual = test.PlayerGrid.HitTile (0, 0);
 			Assert.AreEqual ("hit something!", actual.Text, "should return 'hit something!'");
@@ -32,6 +52,15 @@ namespace MyGame
 		{
 			BattleShipsGame battle = new BattleShipsGame ();
 			Player test = new Player (battle);
+
+
+			//moving ships into invalid positions and making sure they are off the grid
+
+			try { test.PlayerGrid.MoveShip (10, 1, ShipName.Submarine, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 2, ShipName.Destroyer, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 3, ShipName.Battleship, Direction.LeftRight); } catch (Exception ex) { }
+			try { test.PlayerGrid.MoveShip (10, 4, ShipName.AircraftCarrier, Direction.LeftRight); } catch (Exception ex) { }
+
 
 			test.PlayerGrid.MoveShip (0, 0, ShipName.Tug, Direction.LeftRight);
 
