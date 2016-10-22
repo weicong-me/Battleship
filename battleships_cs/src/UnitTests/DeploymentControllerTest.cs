086add5aed7523e34db26a873267f9803fd5ee94
swinericvuong <2076810@student.swin.edu.au>
2016-10-22 21:23:52 +1100

    eric add unit test 2nd try

---
+++ battleships_cs/src/UnitTests/DeploymentControllerTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using SwinGameSDK;
+using NUnit.Framework;
+namespace MyGame
+{
+    [TestFixture()]
+    public class EmptyClass
+    {
+        public EmptyClass ()
+        {
+        }
+		
+        [Test ()]
+        public void TestOrientation ()
+        {
+            Ship sub = new Ship (ShipName.Submarine);
+            Direction _currentdirection = new Direction();
+            _currentdirection = Direction.LeftRight;
+            sub.Deployed (Direction.LeftRight, 5, 5);
+            Assert.IsTrue (sub.Direction == _currentdirection);
+            //Assert.IsTrue (sub.Column == 6);
+        }
+
+        //[Test ()]
+        //public void TestShotCount ()
+        //{
+        //    BattleShipsGame bsg = new BattleShipsGame ();
+        //    Player Jeff = new Player (bsg);
+        //    Ship sub = new Ship (ShipName.Submarine);
+        //    sub.Deployed (Direction.LeftRight, 5, 5);
+        //    //Jeff.Shoot (5, 5);
+        //    Assert.IsTrue (Jeff.Shots == 1);
+        //}
+
+        [Test ()]
+        public void TestShipPlacement ()
+        {
+            Ship sub = new Ship (ShipName.Submarine);
+            sub.Deployed (Direction.LeftRight, 5, 5);
+            Assert.IsTrue (sub.Row == 5);
+        }
+
+        [Test ()]
+        public void TestDIfficulty ()
+        {
+            BattleShipsGame bsg = new BattleShipsGame ();
+            GameController.SetDifficulty (AIOption.Easy);
+            GameController.StartGame ();
+            Assert.IsNull (AIOption.Easy);
+        }
+    }
+}
